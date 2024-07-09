2783de7189a6d9aacf5d0299c2d466535e546c22
Devon Kiss <faktsonly@gmail.com>
2024-07-09 09:50:55 -0500

    Corrected link to GitHub.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return ( 
     <>
       <div className="w-100 text-center">
-        <small><small>Source code: <a href="https://github.com/tuffs/todo-app" target="_blank" style={{color: "palegoldenrod"}}>https://github.com/tuffs/todo-app</a></small></small>
+        <small><small>Source code: <a href="https://github.com/tuffs/todos-app" target="_blank" style={{color: "palegoldenrod"}}>https://github.com/tuffs/todos-app</a></small></small>
       </div>
     </>
 
